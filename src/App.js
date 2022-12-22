50568b07117965eafd737ba30c128a3226e2224b
diegomitari <diego89ma@gmail.com>
2022-12-21 23:38:33 -0500

    responsive

--- src/App.js
+++ src/App.js
@@ -20,35 +20,35 @@ function App() {
           nombre='Shawn Wang'
           pais='Singapur'
           imagen='shawn'
-          cargo='Ingeniero de Software'
+          cargo='Software Engineer'
           empresa='Amazon'
-          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar 
-          trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. 
-          Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. FreeCodeCamp cambió mi vida.'
+          testimonio="It's scary to change careers. I only gained confidence that I could code
+                     by working through the hundreds of hours of free lessons on freeCodeCamp. 
+                     Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
         />
-        
+
         {/* 2nd testimonio */}
         <Testimonio
           nombre='Sarah Chima'
           pais='Nigeria'
           imagen='sarah'
-          cargo='Ingeniera de Software'
+          cargo='Software Engineer'
           empresa='ChatDesk'
-          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. 
-          El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. 
-          Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+          testimonio="freeCodeCamp was the gateway to my career as a software developer. 
+                    The well-structured curriculum took my coding knowledge from a total beginner level to 
+                    a very confident level. It was everything I needed to land my first dev job at an amazing company."
         />
 
-        {/* 3st testimonio */}  
+        {/* 3st testimonio */}
         <Testimonio
           nombre='Emma Bostian'
           pais='Suecia'
           imagen='emma'
           cargo='Ingeniera de Software'
           empresa='Spotify'
-          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp
-           fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp
-            me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+          testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was 
+                    the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me 
+                    the skills and confidence I needed to land my dream job as a software engineer at Spotify."
         />
       </div>
 
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -7,7 +7,7 @@ function Testimonio(props) {
     <div className="contenedor-testimonio">
       <img src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt="Imagen de Emma" className="imagen-testimonio" />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio"><b>{props.nombre}</b> en {props.pais}</p>
+        <p className="nombre-testimonio"><b>{props.nombre}</b> at {props.pais}</p>
         <p className="cargo-testimonio">{props.cargo} en <b>{props.empresa}</b></p>
         <p className="texto-testimonio">{props.testimonio}</p>
       </div>
